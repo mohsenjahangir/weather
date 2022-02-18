4b57a91a5dcbc245d15f4652d771f3a18d8ad9e0
mohsen jahangiri <moh7seven@yahoo.com>
2022-02-18 18:19:55 +0330

    rebuild

--- src/App.js
+++ src/App.js
@@ -3,24 +3,23 @@ const api ={
   key:"3cc6b8a7d755a22b1ebbede9836e6a0e",
   base:"https:api.openweathermap.org/data/2.5/"
 }
-const App=() =>{
-  const {query,setQuery}=useState('');
-  const {weather,setWeather}=useState ({});
+function App(){
+  const [query,setQuery]=useState('');
+  const [weather,setWeather]=useState ({});
   const search=evt=>{
     if(evt.key==="Enter"){
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.base}`)
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
       .then(res=>res.json())
       .then(result=>{
         setWeather(result);
         setQuery('');
         console.log(result);
       });
-  }
+    }}
 
   const dateBuilder=(d)=>{
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    
     let day=days[d.getDay()];
     let date=d.getDate();
     let month=months[d.getMonth()]
